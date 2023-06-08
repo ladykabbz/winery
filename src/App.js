e7bf1dcf59b2ed4fca45e31932703db948cf43f4
ladykabbz <kabiinicole@gmail.com>
2023-06-08 10:55:29 +0300

    add snips

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import Container from "react-bootstrap/Container";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand className="logo" href="#home">
+            ROCKS PLAYHOUSE
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#services">Services</Nav.Link>
+              <Nav.Link href="#gallery">Gallery</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="intro">
+        <div className="container">
+          <h2 className="one">ROCKS PLAYHOUSE</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
