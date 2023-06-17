29b088ea694bfa0b03b7f601a798969eb62ef88a
ladykabbz <kabiinicole@gmail.com>
2023-06-17 13:25:12 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 import Container from "react-bootstrap/Container";
-import RotateExample from "./animes";
+import rocks1 from "./image/rocks1.jpeg";
+import rocks2 from "./image/rocks2.jpeg";
+import rocks4 from "./image/rocks4.jpeg";
+import rocks7 from "./image/rocks7.jpeg";
+import rocks9 from "./image/rocks9.jpg";
+import OrderForm from "./OrderForm";
 
 export default function App() {
   return (
@@ -20,31 +25,217 @@ export default function App() {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#services">Services</Nav.Link>
-              <Nav.Link href="#gallery">Gallery</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#contact">contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      <div className="intro">
+      <div className="intro" id="home">
         <div className="container">
-          <RotateExample />
+          <h2 className="one">
+            BRINGING PEOPLE TOGETHER THROUGH SPONTANEOUS CONVERSATIONS
+          </h2>
         </div>
       </div>
 
       <div className="container">
-        <div className="row">
-          <div className="col-lg-8">
-            <h3 className="about">ALL ABOUT ROCKS PLAYHOUSE</h3>
-            <p className="talkrocks">
-              lorem ipsum bla bla bla bla bla bla bla bla njsdc njs lorem ipsum
-              lorem ipsum bla bla bla bla bla bla bla bla njsdc njs bla bla bla
-              lorem ipsum bla bla bla bla bla bla bla bla njsdc njs bla bla bla
-              bla bla njsdc njs lorem ipsum bla bla bla bla bla bla bla bla
-              njsdc njs
-            </p>
+        <div className="part1">
+          <h2 className="heading3" id="about">
+            ABOUT ROCKS PLAYHOUSE
+          </h2>
+          <div className="row">
+            <div className="col-lg-6">
+              <h3 className="about">-Initiate great conversations through-</h3>
+              <h3 className="subheading">ROCKS PLAYCARDS!</h3>
+
+              <p className="info">
+                Feeling a bit jiggy?or you want to have an exciting gamenight
+                with friends?Perhaps you want to spark up some intense
+                conversation with someone that you are getting to know more?Then
+                Rocks playhouse is the place you should run to.Rocks playcards
+                are specially curated to initiate mature and exciting
+                converations that will have you giving a second of thought into
+                it.
+              </p>
+              <OrderForm />
+            </div>
+            <div className="col-lg-3">
+              <br />
+              <img
+                src={rocks1}
+                alt="nails"
+                width={300}
+                height={200}
+                className="image1"
+              />
+
+              <img
+                src={rocks2}
+                alt="nails"
+                width={250}
+                height={225}
+                className="image2"
+              />
+              <br />
+              <br />
+            </div>
+
+            <div className="col-lg-3">
+              <br />
+              <img
+                src={rocks4}
+                alt="nails"
+                width={275}
+                height={325}
+                className="image3"
+              />
+            </div>
+          </div>
+
+          <br />
+          <br />
+        </div>
+      </div>
+
+      <div className="part2">
+        <div className="container">
+          <h2 className="heading2" id="why">
+            WHY ROCKS PLAYHOUSE?
+          </h2>
+          <div className="row">
+            <div className="col-lg-3">
+              <br />
+              <br />
+
+              <img
+                src={rocks7}
+                alt="nails"
+                width={275}
+                height={325}
+                className="image7"
+              />
+            </div>
+            <div className="col-lg-3">
+              <br />
+              <br />
+
+              <img
+                src={rocks9}
+                alt="nails"
+                width={275}
+                height={325}
+                className="image9"
+              />
+            </div>
+
+            <div className="col-lg-6">
+              <p className="info1">
+                We've gone the extra mile to make these cards totally epic as
+                We've done tons of research and talked to awesome people like
+                you to create the most mind-blowing content ever. These cards
+                are packed with questions and topics that will blow your mind,
+                spark your imagination, and make you see the world in a whole
+                new light.
+              </p>
+              <hr />
+              <p className="info1">
+                We've tapped into the secrets of what gets people talking,
+                laughing, and connecting. So, get ready for some wild
+                discussions that'll leave you amazed by the different
+                perspectives out there. But here's the really cool part: we've
+                made sure Rocks PlayCards are a reflection of YOU. We know
+                you're unique, so we've included challenges and reflections that
+                dig deep into your passions, dreams, and strengths. It's like
+                personal growth on steroids!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="part3">
+        <div className="container">
+          <h2 className="heading3">GET INTO THE PLAYHOUSE</h2>
+          <p className="info3">
+            Grab some conversational cards and get ready to dive into awesome
+            conversations, learn new things, and have a blast connecting with
+            others. It's like a secret weapon for awesome social skills! Get
+            ready to rock those conversations like a pro! Grab Rocks PlayCards,
+            gather your crew, and let the magic happen. It's time to unleash
+            your inner conversational genius and show the world what you're made
+            of. Get ready to rock the talk, my darling!
+          </p>
+          <OrderForm />
+          <br />
+        </div>
+        <br />
+      </div>
+
+      <div className="finalpart" id="contact">
+        <div className="container">
+          <div className="row">
+            <div class="col-lg-4">
+              <ul>
+                <li class="finalist">
+                  <a class="finalink" href="#home">
+                    Home
+                  </a>
+                </li>
+                <li class="finalist">
+                  <a class="finalink" href="#about">
+                    About us
+                  </a>
+                </li>
+                <li class="finalist">
+                  <a class="finalink" href="#why">
+                    why us
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-4">
+              <h3 class="finalname">ROCKS PLAYHOUSE</h3>
+
+              <p className="finalword">
+                BRINGING PEOPLE TOGETHER THROUGH SPONTANEOUS CONVERSATIONS
+              </p>
+              <br />
+            </div>
+
+            <div class="col-lg-4">
+              <ul>
+                <li class="finalist">
+                  <a
+                    class="finalink"
+                    href="mailto:kabiinicole@gmail.com"
+                    target="_blank"
+                  >
+                    Email
+                  </a>
+                </li>
+                <li class="finalist">
+                  <a
+                    class="finalink"
+                    href="https://instagram.com/nicole_kabii?igshid=YmMyMTA2M2Y="
+                    target="_blank"
+                  >
+                    instagram
+                  </a>
+                </li>
+                <li class="finalist">
+                  <a
+                    class="finalink"
+                    href="https://twitter.com/simply_nicoole"
+                    target="_blank"
+                  >
+                    twitter
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/OrderForm.js
@@ -0,0 +1,87 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function OrderForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [numberOfCards, setNumberOfCards] = useState(1);
+  const [showForm, setShowForm] = useState(false);
+  const [totalAmount, setTotalAmount] = useState(1000);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Display the thank you message
+    alert(
+      "Thank you for your order!An Email has been sent for payment details!."
+    );
+
+    // Reset form fields and hide the form
+    setName("");
+    setEmail("");
+    setNumberOfCards(1);
+    setShowForm(false);
+  };
+
+  const handleNumberOfCardsChange = (event) => {
+    const value = parseInt(event.target.value);
+    setNumberOfCards(value);
+    setTotalAmount(value * 1000);
+  };
+
+  return (
+    <div>
+      <button className="btnintro" onClick={() => setShowForm(true)}>
+        TRY ROCKS PLAYCARDS
+      </button>
+
+      {showForm && (
+        <div className="form-overlay">
+          <div className="form-container">
+            <h3 className="orderheading">ORDER ROCKS PLAYCARDS</h3>
+            <form onSubmit={handleFormSubmit}>
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <br />
+              <br />
+
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <br />
+              <br />
+              <label htmlFor="numberOfCards">Number:</label>
+              <input
+                type="number"
+                id="numberOfCards"
+                min="1"
+                value={numberOfCards}
+                onChange={handleNumberOfCardsChange}
+                required
+              />
+
+              <p>Total Amount: {totalAmount} Ksh</p>
+
+              <button className="btnsubmit" type="submit">
+                ORDER
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default OrderForm;
